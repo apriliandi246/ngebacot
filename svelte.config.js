f5856f2aa4b889ae01566fedd992166c6670b04f
Farhan Apriliandi <apriliandi642@gmail.com>
2021-09-15 11:43:08 +0700

    init project

---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import path from "path";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		target: "#svelte",
+
+		vite: {
+			resolve: {
+				alias: {
+					$style: path.resolve("./src/style"),
+					$config: path.resolve("./src/config"),
+					$components: path.resolve("./src/components")
+				}
+			}
+		}
+	}
+};
+
+export default config;
