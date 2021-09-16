8137ce8da11b4bb9350025d6d82033c6d0ff796b
Farhan Apriliandi <apriliandi642@gmail.com>
2021-09-16 10:47:19 +0700

    add some my design system and Firebase

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,12 @@
 	import "$style/global.css";
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+	<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js"></script>
+	<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-firestore.js"></script>
+</svelte:head>
+
 <slot />
